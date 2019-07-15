cdb61f23986a2947aee51bf97aebcdbda5a6d1e8
mfboulos <mboulos95@gmail.com>
2019-07-15 17:44:06 -0400

    Baseline components for navigation

---
+++ src/components/Daily.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class Daily extends React.Component {
+    render() {
+        return <div>Daily Coding Problem</div>
+    }
+}
+
+export default Daily;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import { HashRouter, Route, Link } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Daily from './components/Daily';
+import Resume from './components/Resume';
+import Projects from './components/Projects';
 
 ReactDOM.render((
 <HashRouter>
     <div>
         <Route exact path="/" component={App}/>
+        <Route path="/daily-coding-problem" component={Daily}/>
+        <Route path="/projects" component={Projects}/>
+        <Route path="/resume" component={Resume}/>
     </div>
 </HashRouter>
 ), document.getElementById('root'));
