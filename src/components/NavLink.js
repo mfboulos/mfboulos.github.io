03ea8576b43658a994a84383f937d7db1794d88c
mfboulos <mboulos95@gmail.com>
2019-07-16 01:20:12 -0400

    Finalized nav bar hover functionality

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -2,8 +2,27 @@ import React from 'react';
 import { NavLink as ReactNavLink } from 'react-router-dom';
 
 class NavLink extends React.Component {
+    constructor() {
+        super();
+        this.state = {hover: false};
+        this.toggleHover = this.toggleHover.bind(this);
+    }
+
+    toggleHover() {
+        this.setState({hover: !this.state.hover});
+    }
+
     render() {
-        return <ReactNavLink {...this.props} activeStyle = {{ font: 'bolder' }}/>
+        var navStyle = {'color': '#31cbce', 'textDecoration': 'none'};
+        if (this.state.hover) {
+            navStyle.color = '#72f4fc';
+        }
+        
+        return <ReactNavLink {...this.props}
+            style = {navStyle}
+            activeStyle = {{ 'color': '#b8fafe' }}
+            onMouseEnter = {this.toggleHover}
+            onMouseLeave = {this.toggleHover}/>
     }
 }
 
