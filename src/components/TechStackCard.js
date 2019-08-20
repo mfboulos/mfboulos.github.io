e3bb55e1eb723fead39256c83a129710b94fde9b
mfboulos <mboulos95@gmail.com>
2019-08-20 12:00:32 -0400

    Always niceName the image for a card, that goes to the title

--- src/components/TechStackCard.js
+++ src/components/TechStackCard.js
@@ -5,7 +5,7 @@ class TechStackCard extends React.Component {
     render() {
         return (
             <div style={{height: '130px', display: 'inline-block', borderRadius: '5px', border: '1px solid #888888', margin: '10px'}}>
-                <TechStackIcon style={{height: '100px', ...this.props.style}} tech={this.props.tech} />
+                <TechStackIcon style={{height: '100px', ...this.props.style}} tech={TechStackIcon.niceName(this.props.tech)} />
                 <div style={{height: '0px', margin: 'auto', width: '90%', borderTop: '1px solid #888888'}} />
                 <div style={{height: '30px', textAlign: 'center'}}>{TechStackIcon.niceName(this.props.tech)}</div>
             </div>
