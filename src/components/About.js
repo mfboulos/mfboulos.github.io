615c9c6988f40acee34a3539968bcc1c02923df8
mfboulos <mboulos95@gmail.com>
2019-08-20 13:51:14 -0400

    About Me page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from 'react';
+import './About.css';
 
 class About extends React.Component {
     render() {
         return (
-            <div>
-                <h1>About Me</h1>
-                <p>Hi my name's Michael and I'm a</p>
+            <div style={{fontSize: 20, padding: '30px', paddingTop: '0px'}}>
+                <blockquote>
+                    Eccentric backend-focused software engineer pushing the
+                    boundaries of performance and the impossible, building great
+                    things with great people.
+                </blockquote>
+                <div className='bio'>
+                    I have always been a fiend for challenges,
+                    puzzles, and marginal optimizations. This passion initially
+                    drove me to studying math, but then I had the opportunity to
+                    print <code>"Hello World!"</code> and my career trajectory
+                    changed instantly. 
+                </div>
+                <div className='bio'>
+                    The transition was easy, fluid, and meaningful;
+                    I kicked off the ground running, and got to work with and mentor
+                    some of the brightest upcoming minds in computer science. I am a
+                    serious believer in the value of discourse and surrounding myself
+                    with people I can learn from.
+                </div>
+                <blockquote>
+                    If you are the smartest person in the room, then you are in the wrong room.
+                </blockquote>
+                <div className='bio'>
+                    As a software engineer, I take full advantage of my
+                    mathematical background to model solutions instantly. I take pride
+                    in my work, and I am very excited to work with other engineers
+                    that share my passion for challenges and excellence.
+                </div>
             </div>
         )
     }
