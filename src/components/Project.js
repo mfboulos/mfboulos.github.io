1c9531714bd795c4c7458ecf498c6861d32e37c3
mfboulos <mboulos95@gmail.com>
2019-09-12 18:33:45 -0700

    Style differences based on window width

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,6 +5,48 @@ import TechStackCard from './TechStackCard';
 
 class Project extends React.Component {
     render() {
+        let descAndDemo = window.innerWidth > 600
+            ? <div style={{display: 'grid', gridTemplateColumns: '60% 10% 30%'}}>
+                <div style={{gridColumnEnd: 2}}>{this.props.display.description}</div>
+                {this.props.display.demo
+                ? <img style={{maxWidth: '100%', minWidth: '150px' , marginLeft: 'auto', height: 'auto', gridColumnStart: 3}}
+                alt='Demo'
+                src={'https://raw.githubusercontent.com/mfboulos/'
+                + `${this.props.project.name}/${this.props.project.default_branch}/${this.props.display.demo}`} />
+                : null}
+            </div>
+        : <div>
+            {this.props.display.demo
+            ? <img style={{maxWidth: '100%', minWidth: '150px' , marginLeft: 'auto', height: 'auto', marginBottom: '20px'}}
+            alt='Demo'
+            src={'https://raw.githubusercontent.com/mfboulos/'
+            + `${this.props.project.name}/${this.props.project.default_branch}/${this.props.display.demo}`} />
+            : null}
+            <div>{this.props.display.description}</div>
+        </div>
+
+        let featuresAndStack = window.innerWidth > 600
+        ? <div style={{marginTop: '20px', display: 'grid', gridTemplateColumns: '45% 10% 45%'}}>
+            <div style={{gridColumnEnd: 2}}>
+                <h3>Features:</h3>
+                {this.props.display.features.map((feature, index) => <li key={index}>{feature}</li>)}
+            </div>
+            <div style={{gridColumnStart: 3}}>
+                <h3>Tech Stack:</h3>
+                {this.props.display.stack.map((t, index) => <TechStackCard key={index} tech={t} />)}
+            </div>
+        </div>
+        : <div>
+            <div style={{marginTop: '20px'}}>
+                <h3>Features:</h3>
+                {this.props.display.features.map((feature, index) => <li key={index}>{feature}</li>)}
+            </div>
+            <div style={{marginTop: '20px'}}>
+                <h3>Tech Stack:</h3>
+                {this.props.display.stack.map((t, index) => <TechStackCard key={index} tech={t} />)}
+            </div>
+        </div>
+
         return (
             <Accordion style={{padding: '3px'}} defaultActiveKey='abc'>
                 <Card style={{borderRadius: '5px', backgroundColor: '#444444'}}>
@@ -15,25 +57,8 @@ class Project extends React.Component {
                 
                     <Accordion.Collapse>
                         <Card.Body style={{backgroundColor: '#616161'}}>
-                            <div style={{display: 'grid', gridTemplateColumns: '60% 10% 30%'}}>
-                                <div style={{gridColumnEnd: 2}}>{this.props.display.description}</div>
-                                {this.props.display.demo
-                                ? <img style={{maxWidth: '100%', minWidth: '150px' , marginLeft: 'auto', height: 'auto', gridColumnStart: 3}}
-                                alt='Demo'
-                                src={'https://raw.githubusercontent.com/mfboulos/'
-                                + `${this.props.project.name}/${this.props.project.default_branch}/${this.props.display.demo}`} />
-                                : null}
-                            </div>
-                            <div style={{marginTop: '20px', display: 'grid', gridTemplateColumns: '45% 10% 45%'}}>
-                                <div style={{gridColumnEnd: 2}}>
-                                    <h3>Features:</h3>
-                                    {this.props.display.features.map((feature, index) => <li key={index}>{feature}</li>)}
-                                </div>
-                                <div style={{gridColumnStart: 3}}>
-                                    <h3>Tech Stack:</h3>
-                                    {this.props.display.stack.map((t, index) => <TechStackCard key={index} tech={t} />)}
-                                </div>
-                            </div>
+                            {descAndDemo}
+                            {featuresAndStack}
                             <div style={{marginTop: '20px'}}>
                                 <GithubButton url={this.props.project.html_url} />
                             </div>
--- src/components/WorkProject.js
+++ src/components/WorkProject.js
@@ -4,32 +4,61 @@ import TechStackCard from './TechStackCard';
 
 class WorkProject extends React.Component {
     render() {
+        let isWideEnough = window.innerWidth > 720
+        let rolesAndAccomplishments = isWideEnough
+        ? <div style={{display: 'grid', gridTemplateColumns: '45% 5% 45% 5%'}}>
+            <div style={{gridColumnEnd: 2}}>
+                <h3>Roles:</h3>
+                <ul>
+                    {this.props.roles.map((role, index) => <li key={index}>{role}</li>)}
+                </ul>
+            </div>
+            <div style={{gridColumnStart: 3, gridColumnEnd: 4}}>
+                <h3>Accomplishments:</h3>
+                <ul>
+                    {this.props.accomplishments.map((acc, index) => <li key={index}>{acc}</li>)}
+                </ul>
+            </div>
+        </div>
+        : <div>
+            <div>
+                <h3>Roles:</h3>
+                <ul>
+                    {this.props.roles.map((role, index) => <li key={index}>{role}</li>)}
+                </ul>
+            </div>
+            <div style={{marginTop: '20px'}}>
+                <h3>Accomplishments:</h3>
+                <ul>
+                    {this.props.accomplishments.map((acc, index) => <li key={index}>{acc}</li>)}
+                </ul>
+            </div>
+        </div>
+
+        let positionAndDates = isWideEnough
+            ? <div>
+                <div style={{position: 'absolute', top: '15px', right: '20px'}}>{this.props.role}</div>
+                <div style={{position: 'absolute', top: '40px', right: '20px', color: '#dddddd',
+                maxWidth: '25%', textAlign: 'right', fontSize: 12}}>{this.props.start} - {this.props.end}</div>
+            </div>
+            : <div>
+                <div style={{marginTop: '20px'}}>{this.props.role}</div>
+                <div style={{color: '#dddddd', fontSize: 12}}>{this.props.start} - {this.props.end}</div>
+            </div>
+
         return (
             <Accordion style={{padding: '3px'}}>
                 <Card style={{borderRadius: '5px', backgroundColor: '#444444'}}>
                     <Accordion.Toggle style={{cursor: 'pointer'}} as={Card.Header} variant='link' eventKey={this.props.id}>
                         <div style={{fontSize: 20}}>{this.props.name}</div>
-                        <div style={{color: '#dddddd', fontSize: 14, maxWidth: '70%'}}>{this.props.shortDescription}</div>
-                        <div style={{position: 'absolute', top: '15px', right: '20px'}}>{this.props.role}</div>
-                        <div style={{position: 'absolute', top: '40px', right: '20px', color: '#dddddd', maxWidth: '25%', textAlign: 'right'}}>{this.props.start} - {this.props.end}</div>
+                        <div style={{color: '#dddddd', fontSize: 12,
+                        maxWidth: isWideEnough ? '70%' : '100%'}}>{this.props.shortDescription}</div>
+                        {positionAndDates}
                     </Accordion.Toggle>
                 
                     <Accordion.Collapse>
                         <Card.Body style={{backgroundColor: '#616161'}}>
-                            <div style={{display: 'grid', gridTemplateColumns: '45% 5% 45% 5%'}}>
-                                <div style={{gridColumnEnd: 2}}>
-                                    <h3>Roles:</h3>
-                                    <ul>
-                                        {this.props.roles.map((role, index) => <li key={index}>{role}</li>)}
-                                    </ul>
-                                </div>
-                                <div style={{gridColumnStart: 3, gridColumnEnd: 4}}>
-                                    <h3>Accomplishments:</h3>
-                                    <ul>
-                                        {this.props.accomplishments.map((acc, index) => <li key={index}>{acc}</li>)}
-                                    </ul>
-                                </div>
-                            </div>
+                            {rolesAndAccomplishments}
                             <div style={{marginTop: '20px'}}>
                                 <h3>Tech Stack</h3>
                                 {this.props.techStack.map((tech, index) => <TechStackCard key={index} tech={tech} />)}
