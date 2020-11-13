2bc551094c3b6e3ea92cc31a225c84685bce375f
mfboulos <mboulos95@gmail.com>
2020-11-12 20:48:27 -0800

    Move gh project metadata to config

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,43 +6,43 @@ import TechStackCard from './TechStackCard';
 export default (props) => {
     let descAndDemo = window.innerWidth > 600
         ? <div style={{display: 'grid', gridTemplateColumns: '60% 10% 30%'}}>
-            <div style={{gridColumnEnd: 2}}>{props.display.description}</div>
-            {props.display.demo
+            <div style={{gridColumnEnd: 2}}>{props.data.description}</div>
+            {props.data.demo
             ? <img style={{maxWidth: '100%', minWidth: '150px' , marginLeft: 'auto', height: 'auto', gridColumnStart: 3}}
             alt='Demo'
             src={'https://raw.githubusercontent.com/mfboulos/'
-            + `${props.project.name}/${props.project.default_branch}/${props.display.demo}`} />
+            + `${props.project.name}/${props.project.default_branch}/${props.data.demo}`} />
             : null}
         </div>
     : <div>
-        {props.display.demo
+        {props.data.demo
         ? <img style={{maxWidth: '100%', minWidth: '150px' , marginLeft: 'auto', height: 'auto', marginBottom: '20px'}}
         alt='Demo'
         src={'https://raw.githubusercontent.com/mfboulos/'
-        + `${props.project.name}/${props.project.default_branch}/${props.display.demo}`} />
+        + `${props.project.name}/${props.project.default_branch}/${props.data.demo}`} />
         : null}
-        <div>{props.display.description}</div>
+        <div>{props.data.description}</div>
     </div>
 
     let featuresAndStack = window.innerWidth > 600
     ? <div style={{marginTop: '20px', display: 'grid', gridTemplateColumns: '45% 10% 45%'}}>
         <div style={{gridColumnEnd: 2}}>
             <h3>Features:</h3>
-            {props.display.features.map((feature, index) => <li key={index}>{feature}</li>)}
+            {props.data.features.map((feature, index) => <li key={index}>{feature}</li>)}
         </div>
         <div style={{gridColumnStart: 3}}>
             <h3>Tech Stack:</h3>
-            {props.display.stack.map((t, index) => <TechStackCard key={index} tech={t} />)}
+            {props.data.stack.map((t, index) => <TechStackCard key={index} tech={t} />)}
         </div>
     </div>
     : <div>
         <div style={{marginTop: '20px'}}>
             <h3>Features:</h3>
-            {props.display.features.map((feature, index) => <li key={index}>{feature}</li>)}
+            {props.data.features.map((feature, index) => <li key={index}>{feature}</li>)}
         </div>
         <div style={{marginTop: '20px'}}>
             <h3>Tech Stack:</h3>
-            {props.display.stack.map((t, index) => <TechStackCard key={index} tech={t} />)}
+            {props.data.stack.map((t, index) => <TechStackCard key={index} tech={t} />)}
         </div>
     </div>
 
@@ -50,7 +50,7 @@ export default (props) => {
         <Accordion style={{padding: '3px'}} defaultActiveKey='abc'>
             <Card style={{borderRadius: '5px', backgroundColor: '#444444'}}>
                 <Accordion.Toggle style={{cursor: 'pointer'}} as={Card.Header} variant='link' eventKey={props.id}>
-                    <div style={{fontSize: 20}}>{props.display.name}</div>
+                    <div style={{fontSize: 20}}>{props.data.displayName}</div>
                     <div style={{color: '#dddddd',fontSize: 14}}>{props.project.description}</div>
                 </Accordion.Toggle>
             
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,48 +2,93 @@ import React from 'react'
 import Project from './Project';
 import LoadingIcon from './Loading'
 
-class Projects extends React.Component {
-    projects = []
+const projectData = [
+    {
+        "name": "downtime-alerter",
+        "displayName": "SMS Downtime Alerter",
+        "description": "Application that watches a URL, and notifies by SMS using the Twilio API as soon as it goes down. This was primarily developed to monitor the health of this portfolio while being hosted by Github Pages, but any URL that can handle a GET request can be monitored.",
+        "features": [
+            "Periodic URL health monitoring",
+            "SMS notification with Twilio API",
+            "Periodic reminder notifications during extended downtime"
+        ],
+        "stack": [
+            "Python", "Twilio"
+        ]
+    },
+    {
+        "name": "document-storage",
+        "displayName": "Document Storage Service",
+        "description": "ID-based document storage and retrieval REST web service. Files are saved locally and referenced by a unique 20-character alphanumeric ID, through which they can be retrieved, updated, or deleted. File metadata (original file name/extension, file size) is stored in an in-memory H2 database, although the application can easily be configured to connect to a different SQL driver and database.",
+        "features": [
+            "Support for all file types",
+            "SQL in-memory database metadata storage",
+            "Local file storage with assigned unique ID through HTTP POST multipart form-data",
+            "File retrieval by ID through HTTP GET",
+            "File updation by ID through HTTP UPDATE",
+            "File removal by ID through HTTP DELETE"
+        ],
+        "stack": [
+            "Java", "Spring", "Hibernate", "SQL", "H2"
+        ]
+    },
+    {
+        "name": "3D-Surface-Viewer",
+        "displayName": "OpenGL 3D Surface Viewer",
+        "description": "OpenGL Surface Viewer for two-variable mathematical functions. The motivation is to improve on most available implementations of this idea by offering a more flexible way for users to view the functions they are working with, without having to worry about trivial things like large numbers or division by zero.",
+        "features": [
+            "User-defined function evaluation",
+            "Camera rotation, pan, and zoom",
+            "X and Y bounds",
+            "Render resolution",
+            "Output (Z) bounds",
+            "Graceful handling of infinity and division by zero"
+        ],
+        "stack": [
+            "OpenGL", "C++", "ExprTk"
+        ],
+        "demo": "demo.gif"
+    }
+]
 
+class Projects extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            isLoading: true
+            isLoading: true,
+            projects: []
         }
     }
 
     componentDidMount() {
-        fetch('https://api.github.com/users/mfboulos/repos')
-        .then(res => {
-            if (res.ok) {
+        let amountDone = 0
+        let projectMap = new Map()
+        projectData.forEach(data => {
+            fetch(`https://api.github.com/repos/mfboulos/${data.name}`)
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error('Error retrieving repo')
+                }
                 return res.json()
-            }
-            throw new Error('There was a problem retrieving repos!')
-        })
-        .then((data) => {
-            let amountDone = 0
-            data.sort((d1, d2) => new Date(d2.updated_at) - new Date(d1.updated_at))
-            data.filter(d => !d.private).forEach(project => {
-                fetch(`https://raw.githubusercontent.com/mfboulos/${project.name}/${project.default_branch}/boulos.json`)
-                .then(res => {
-                    if (!res.ok) {
-                        throw new Error('Repo has no display properties!')
-                    }
-                    return res.json()
-                })
-                .then(data => {
-                    this.projects.push(<Project key={project.id} project={project} display={data}></Project>)
-                })
-                .catch(err => {})
-                .finally(() => {
-                    amountDone++
-                    if (amountDone === data.filter(d => !d.private).length) {
-                        this.setState({'isLoading': false})
-                    }
-                })
+            })
+            .then(project => {
+                projectMap.set(data, project)
+            })
+            .catch(err => console.log(err.message))
+            .finally(() => {
+                amountDone++
+                if (amountDone === projectData.length) {
+                    this.setState({
+                        isLoading: false,
+                        projects: [...projectMap.entries()].sort((a, b) => new Date(b[1].updated_at) - new Date(a[1].updated_at)).map(entry => {
+                            const data = entry[0]
+                            const project = entry[1]
+                            return <Project key={project.id} project={project} data={data} />
+                        })
+                    })
+                }
             })
         })
-        .catch(err => console.log(err.message));
     }
 
     render() {
@@ -52,7 +97,7 @@ class Projects extends React.Component {
                 <h1>Projects</h1>
                 {this.state.isLoading
                     ? <LoadingIcon/>
-                    : <div style={{margin: Math.min(window.innerWidth / 20, 30) + 'px'}}>{this.projects}</div>}
+                    : <div style={{margin: Math.min(window.innerWidth / 20, 30) + 'px'}}>{this.state.projects}</div>}
             </div>
         )
     }
